c9c14148d74f930ebe94d9995476a67fd6e5a228
Josh Mejia <joshua.mejia@gmail.com>
2017-11-18 20:29:52 -0600

    Remove dependency on more_orders helper in order_items_spec

--- spec/models/order_items_spec.rb
+++ spec/models/order_items_spec.rb
@@ -1,22 +1,49 @@
 require 'rails_helper'
 
 describe "order item methods" do
-  before(:each) do
-    more_orders
-  end
-
-  it "can return the sum of all quantity" do
-    quantity_hash = OrderItem.sum_quantity.all.count
-    expected_result = {8=>1, 1=>1, 5=>1, 11=>1, 3=>1, 12=>1, 9=>1, 7=>1}
+  it "returns the quantity for an item associated with an order" do
+    item_1 = create(:item)
+    item_2 = create(:item)
+    order_1 = create(:order, items: [item_1, item_2])
+    order_2 = create(:order, items: [item_1])
+    expected_result = {item_1.id => 2, item_2.id => 1}
+    quantity_hash = OrderItem.sum_quantity.count
     expect(quantity_hash).to eq(expected_result)
   end
 
   it "can return top three items" do
-    expect(OrderItem.top_three_items.count).to eq(3)
-    expect(OrderItem.top_three_items.first.title).to eq("Adult Penguin")
+    item_1 = create(:item)
+    item_2 = create(:item)
+    item_3 = create(:item)
+    item_4 = create(:item)
+    order_1 = create(:order)
+    order_2 = create(:order)
+
+    2.times do
+      order_1.items << item_1
+      order_2.items << item_2
+      order_2.items << item_4
+    end
+
+    order_2.items << item_3
+
+    top_items = OrderItem.top_three_items
+    expect(top_items.count).to eq(3)
+    expect(top_items).to include(item_1)
+    expect(top_items).to include(item_2)
+    expect(top_items).to include(item_4)
+    expect(top_items).to_not include(item_3)
   end
 
   it "can average quantity" do
+    item_1 = create(:item)
+    item_2 = create(:item)
+    order_1 = create(:order)
+    order_2 = create(:order)
+
+    OrderItem.create(item: item_1, quantity: 1, order: order_1)
+    OrderItem.create(item: item_2, quantity: 3, order: order_2)
+
     expect(OrderItem.average_quantity).to eq(2)
   end
 end
