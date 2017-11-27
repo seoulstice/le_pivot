3b83e5053750556df00d4f791a27f853ace62496
Josh Mejia <joshua.mejia@gmail.com>
2017-11-27 15:18:24 -0700

    Whitespace adjustment on routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
-  
+
   get '/login', :to => 'sessions#new', :as => 'login'
   post '/login', :to => 'sessions#create'
   delete '/logout', :to => 'sessions#destroy'
