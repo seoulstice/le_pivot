8e16d7925215cd814f50ca7ce28e92a97284e3c6
Josh Mejia <joshua.mejia@gmail.com>
2017-12-04 13:37:43 -0700

    Update features/admin/items specs

--- spec/features/admin/items/admin_can_create_items_spec.rb
+++ spec/features/admin/items/admin_can_create_items_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.feature "Admin item creation" do
   context "As an authenticated admin" do
     it "I can create an item" do
-      admin = User.create(first_name: "Mimi", last_name: "Le", email: "mimi@mimi.com", password: "mimi", role: 1)
+      admin = create(:admin)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
       visit admin_items_path
@@ -15,7 +15,7 @@ RSpec.feature "Admin item creation" do
       fill_in "item[title]", with: "Onesie"
       fill_in "item[description]", with: "This Onesie is awesome!"
       fill_in "item[price]", with: "59.99"
-      page.attach_file("item[image]", Rails.root + 'app/assets/images/alien-onsie.png') 
+      page.attach_file("item[image]", testing_image)
       click_on "Create Item"
 
       expect(current_path).to eq(admin_items_path)
@@ -23,7 +23,8 @@ RSpec.feature "Admin item creation" do
       expect(page).to have_content("59.99")
     end
     it "I can create an item without an image and it defaults" do
-      admin = User.create(first_name: "Mimi", last_name: "Le", email: "mimi@mimi.com", password: "mimi", role: 1)
+      # admin = User.create(first_name: "Mimi", last_name: "Le", email: "mimi@mimi.com", password: "mimi", role: 1)
+      admin = create(:admin)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
       visit admin_items_path
--- spec/features/admin/items/admin_can_visit_admin_items_and_edit_an_item_spec.rb
+++ spec/features/admin/items/admin_can_visit_admin_items_and_edit_an_item_spec.rb
@@ -2,21 +2,22 @@ require 'rails_helper'
 
 
 RSpec.describe 'an admin can visit admin dashboard' do
-	describe 'and see a link for all items' do
-		it 'when clicked that link should be the admin item index with admin functionality' do
-			admin_user = User.create(first_name: "Admin", last_name: "McAdmin", email: "admin@admin.com", password: "boom", role: "admin")
-			allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
-			create_items			
-			visit admin_items_path
-			click_on "Edit"
-			fill_in "item[title]", with: "White Cat Twosie"
-			fill_in "item[description]", with: "two is better"
-			fill_in "item[price]", with: "39.99"
-			page.attach_file("item[image]", Rails.root + 'app/assets/images/alien-onsie.png') 
+  describe 'and see a link for all items' do
+    it 'when clicked that link should be the admin item index with admin functionality' do
+      create(:item)
+      admin = create(:admin)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
 
-			click_on "Update"
-			expect(page).to have_content("White Cat Twosie")
-			expect(page).to have_content("39.99")
-		end
-	end
+      visit admin_items_path
+      click_on "Edit"
+      fill_in "item[title]", with: "White Cat Twosie"
+      fill_in "item[description]", with: "two is better"
+      fill_in "item[price]", with: "39.99"
+      page.attach_file("item[image]", testing_image)
+      click_on "Update"
+
+      expect(page).to have_content("White Cat Twosie")
+      expect(page).to have_content("39.99")
+    end
+  end
 end
