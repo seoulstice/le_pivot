9714b02cc664fa15de565606713d486f5ce5fa4f
Josh Mejia <joshua.mejia@gmail.com>
2017-12-04 13:36:30 -0700

    Create admin user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,5 +5,13 @@ FactoryBot.define do
     password "password"
     sequence(:email) {|n| "gob#{n}@example.com" }
   end
+
+  factory :admin, class: User do
+    first_name "Gob"
+    last_name "Bluth"
+    password "password"
+    role "admin"
+    sequence(:email) {|n| "gob#{n}@example.com" }
+  end
 end
 
