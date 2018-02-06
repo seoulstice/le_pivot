2dc92c8ed5707587dff2a6147b73eb4669b26adc
seoulstice <jungyoungjoon@gmail.com>
2018-02-06 16:28:00 -0700

    Create order_creator PORO

---
+++ app/models/order_creator.rb
@@ -0,0 +1,11 @@
+class OrderCreator
+
+
+  def create_order(user, cart)
+    order = Order.create(total_price: cart.total_price, user: user)
+    cart.items.each do |item|
+      OrderItem.create(item: item, order: order, quantity: item.quantity)
+    end
+  end
+
+end
