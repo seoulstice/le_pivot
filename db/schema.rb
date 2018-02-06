923c1f11232a1713967a7758a38c80f171b04710
seoulstice <jungyoungjoon@gmail.com>
2018-02-06 13:49:12 -0700

    Create/ran migration to add total_price column to Orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918203915) do
+ActiveRecord::Schema.define(version: 20180206204751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,7 @@ ActiveRecord::Schema.define(version: 20170918203915) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "status"
+    t.integer "status", default: 0
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170918203915) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+    t.float "total_price"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
