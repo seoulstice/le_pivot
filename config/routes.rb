e81d78658299689b9951f35248aaed81d7b5eb1a
seoulstice <jungyoungjoon@gmail.com>
2018-02-06 16:35:34 -0700

    Refactor order routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
 
   resources :users , only: [:new, :create, :edit, :update]
 
-  resources :orders, only: [:index, :new, :show, :update]
+  resources :orders, only: [:index, :create, :show, :update]
 
   resources :dashboard, only: [:index]
 
