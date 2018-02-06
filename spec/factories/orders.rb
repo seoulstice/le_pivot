fdffebe44a69268961e72712e63cc88d368d4989
seoulstice <jungyoungjoon@gmail.com>
2018-02-06 13:45:25 -0700

    Refactor order factory

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,21 +1,6 @@
 FactoryBot.define do
   factory :order do
-    status 1
+    status 0
     user
-
-    transient do
-      items_with_quantity [{create(:item) => 1}]
-    end
-
-    factory :order_with_items do
-      after(:create) do |order, evaluator|
-        evaluator.items_with_quantity.each do |item_with_quantity|
-          item     = item_with_quantity.keys.first
-          quantity = item_with_quantity.values.first
-          create(:order_item, order: order, item: item, quantity: quantity)
-        end
-      end
-    end
-
   end
 end
