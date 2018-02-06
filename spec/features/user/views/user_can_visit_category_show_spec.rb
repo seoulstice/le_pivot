17ade1f0b72d9552b266d6746ace3c859f999f10
seoulstice <jungyoungjoon@gmail.com>
2018-02-06 11:35:02 -0700

    skipped passing test for later rewrite

--- spec/features/user/views/user_can_visit_category_show_spec.rb
+++ spec/features/user/views/user_can_visit_category_show_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 RSpec.describe "As a visitor can visit category show page" do
-    it " can visit category show page" do
+    xit " can visit category show page" do
         create_items
-        
+
         visit '/cats'
-        
+
     end
 end
