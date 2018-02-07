2a81f5c432186d357f6531c1c49087af8e18fc0f
Josi McClellan <josiahmcclellan07@gmail.com>
2018-02-07 13:24:06 -0700

    fix migration name

---
+++ db/migrate/20180207073505_create_api_keys.rb
@@ -0,0 +1,10 @@
+class CreateApiKeys < ActiveRecord::Migration[5.1]
+  def change
+    create_table :api_keys do |t|
+
+      t.string      :key,     null: false, index: { unique: true }
+      t.belongs_to  :user,    null: true,  index: { unique: true }
+
+    end
+  end
+end
