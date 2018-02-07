c986cc676e181da2374375063be1ba724e316530
seoulstice <jungyoungjoon@gmail.com>
2018-02-06 17:03:54 -0700

    Removed/refactored methods out of Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,20 +3,11 @@ class Order < ApplicationRecord
   validates :status, presence: true
   has_many :order_items
   has_many :items, through: :order_items
-  after_save :save_total_price
 
   enum status: ["ordered", "paid", "cancelled", "completed"]
 
   def find_quantity(item)
     OrderItem.find_by(item: item, order: self).quantity
-  end
-
-  def add(item_hash)
-    item_hash.each do |item, quantity|
-      items << item
-      order_item = OrderItem.find_by(order: self, item_id: item.id)
-      order_item.update(quantity: quantity)
-    end
   end
 
   def date
@@ -32,10 +23,10 @@ class Order < ApplicationRecord
   end
 
   def self.count_of_completed_orders
-    where(status: :completed).count
+    completed.count
   end
 
   def self.shop_total_gross
-		where(status: :completed).joins(:items).sum(:price)
+		completed.joins(:items).sum(:price)
   end
 end
