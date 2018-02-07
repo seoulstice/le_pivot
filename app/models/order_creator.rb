7ef7b3847f1b9555051c30c2b730c17e375eea41
seoulstice <jungyoungjoon@gmail.com>
2018-02-07 14:12:53 -0700

    Refactor @cart in application_controller to cart

--- app/models/order_creator.rb
+++ app/models/order_creator.rb
@@ -1,7 +1,7 @@
 class OrderCreator
 
 
-  def create_order(user, cart)
+  def self.create_order(user, cart)
     order = Order.create(total_price: cart.total_price, user: user)
     cart.items.each do |item|
       OrderItem.create(item: item, order: order, quantity: item.quantity)
