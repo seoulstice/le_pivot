7e0c17c79ab0134c3bb6dce9d5c7e86e1cf21b26
seoulstice <jungyoungjoon@gmail.com>
2018-02-07 16:26:21 -0700

    Refactor .create_order method

--- app/models/order_creator.rb
+++ app/models/order_creator.rb
@@ -4,7 +4,7 @@ class OrderCreator
   def self.create_order(user, cart)
     order = Order.create(total_price: cart.total_price, user: user)
     cart.items.each do |item|
-      OrderItem.create(item: item, order: order, quantity: item.quantity)
+      OrderItem.create(item_id: item.id, order: order, quantity: item.quantity)
     end
     order
   end
