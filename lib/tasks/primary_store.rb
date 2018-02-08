1e0075b29e42082b56c6e9697e163e9fda8ba00e
abarnes26 <abarnes26@yahoo.com>
2018-02-08 15:40:21 -0700

    modified rake tasks

--- lib/tasks/primary_store.rb
+++ lib/tasks/primary_store.rb
@@ -1,4 +1,4 @@
 desc 'Create store to represent the original little shop store'
 task :create_first_store do
-  Store.create(name: "The Little Shop")
+  Store.create(name: "The Little Shop of Funsies")
 end
