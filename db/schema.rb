1dc5d59ead47da34c211fdfd4d13387840103ff1
seoulstice <jungyoungjoon@gmail.com>
2018-02-07 17:35:02 -0700

    Ran api_keys migration

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,7 @@ ActiveRecord::Schema.define(version: 20180207073505) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "status"
+    t.integer "status", default: 0
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
