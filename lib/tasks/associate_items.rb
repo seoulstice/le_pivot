04bebb44c24f93ea149e8a8a9a4fe4dee3532b5e
abarnes26 <abarnes26@yahoo.com>
2018-02-08 16:29:34 -0700

    syncing with master

--- lib/tasks/associate_items.rb
+++
@@ -1,8 +0,0 @@
-desc 'After original store is created, associate all current items with that store'
-task :associate_items do
-  original_store_id = Store.first.id
-  Item.all.each do |item|
-    item.store_id = original_store_id
-    item.save
-  end
-end
