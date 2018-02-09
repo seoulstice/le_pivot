acd58fbd7612e678842e61e8d7b1e5c78f36e291
seoulstice <jungyoungjoon@gmail.com>
2018-02-08 18:03:13 -0700

    Create spec for user can create store

---
+++ spec/features/user/stores/user_can_create_store_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+describe "When visiting create store form" do
+  context "as a registered User" do
+    it "a user can create a store" do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+      visit new_store_path
+      fill_in "store[name]", with: "Vandelay Industries"
+      click_on "Submit"
+
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_link("My Stores")
+
+      click_on "My Stores"
+# save_and_open_page
+      expect(page).to have_content("Vandelay Industries")
+      expect(page).to have_content("pending")
+    end
+  end
+end
