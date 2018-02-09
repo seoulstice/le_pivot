18dda2fcf296c9517a8ddbc384cdb03533995999
Josi McClellan <josiahmcclellan07@gmail.com>
2018-02-09 11:35:57 -0700

    add Permission and update application_controller

---
+++ app/services/permission.rb
@@ -0,0 +1,92 @@
+class Permission
+
+  def self.granted?(*args)
+     new(*args).granted?
+  end
+
+  attr_reader :controller, :action, :user
+  def initialize(controller, action, user)
+    @controller = controller.to_sym
+    @action = action.to_sym
+    @user = user || User.new
+  end
+
+  def granted?
+    allowed = permitted.dig(controller, action) || []
+    allowed.equal?(true) || user_has_role_in?(allowed)
+  rescue Exception
+    return false
+  end
+
+  def user_has_role_in?(allowed)
+    roles = user.roles.map(&:to_sym)
+    # single `&` takes the intersection
+    !(roles & allowed).empty?
+  end
+
+  def permitted
+  # @@ because this should be the same everywhere always
+    @@permitted ||= {
+      carts: {
+        show: true,
+        create: true,
+        update: true,
+        destroy: true
+      },
+      categories: {
+        show: true
+      },
+      dashboards: {
+        show: true
+      },
+      developers: {
+        show: true,
+        update: true,
+        create: true,
+      },
+      items: {
+        index: true,
+        show: true
+      },
+      main: {
+        index: true,
+      },
+      orders: {
+        index: true,
+        show: true,
+        update: true
+      },
+      sessions: {
+        new: true,
+        create: true,
+        destroy: true
+      },
+      twitter: {
+        new:    %i{ store_admin platform_admin },
+        create: %i{ store_admin platform_admin },
+        update: %i{ store_admin platform_admin }
+      },
+      users: {
+        new: true,
+        create: true,
+        edit: true,
+        update: true
+      },
+
+      'admin/dashboards': {
+        show: :platform_admin
+      },
+      'admin/analytics': {
+        show: :platform_admin
+      },
+      'admin/items': {
+        index: :platform_admin,
+        new: :platform_admin,
+        create: :platform_admin,
+        update: :platform_admin,
+        edit: :platform_admin
+      }
+    }
+  end
+
+end
