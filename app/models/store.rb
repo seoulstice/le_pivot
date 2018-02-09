bddab45006053a360fad9f71c6849d98045e100b
Josi McClellan <josiahmcclellan07@gmail.com>
2018-02-09 12:33:28 -0700

    update models

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,6 +2,7 @@ class Store < ApplicationRecord
   has_many :items
   has_many :user_roles
 
-  enum status: ["pending", "active", "suspended"]
+  enum status: ['pending', 'active', 'suspended']
 
+  validates_presence_of :name
 end
