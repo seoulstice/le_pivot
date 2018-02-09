6464d4867194354a961a857bc41fce71b106cd75
Josi McClellan <josiahmcclellan07@gmail.com>
2018-02-09 12:19:47 -0700

    update controllers

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
 	def show
-			@category = Category.friendly.find(params[:category_slug])
+		@category = Category.friendly.find(params[:category_slug])
 	end
 end
