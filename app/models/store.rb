4df3370850c45834ee2842f8ec3eb82fc3d82f32
seoulstice <jungyoungjoon@gmail.com>
2018-02-10 14:52:00 -0700

    Create #active_item method

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,8 +1,12 @@
 class Store < ApplicationRecord
   has_many :items
   has_many :user_roles
-
   enum status: ['pending', 'active', 'suspended']
 
   validates_presence_of :name
+
+  def active_items
+    items.active
+  end
+
 end
