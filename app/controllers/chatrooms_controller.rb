61b7191a30a5e195c3b15c14bccf0380228e7c39
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-10 15:36:57 -0700

    create functionality for creating a new chat, chat index

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,8 +1,32 @@
 class ChatroomsController < ApplicationController
 
+  def index
+    @chatrooms = Chatroom.all
+  end
+
   def show
     @chatroom = Chatroom.find_by(slug: params[:slug])
     @message  = Message.new
   end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    if @chatroom.save
+      @chatroom.update(slug: params[:topic].parameterize)
+      redirect_to support_path(@chatroom.slug)
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def chatroom_params
+      params.permit(:chatroom).require(:topic
+    end
 
 end
