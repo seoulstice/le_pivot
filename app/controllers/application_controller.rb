e291a5030152eb97fb13ef5560e467330c629aec
Josi McClellan <josiahmcclellan07@gmail.com>
2018-02-10 11:10:11 -0700

    continue cleaning updating view vars/helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authorize!
 
-<<<<<<< HEAD
-  helper_method :current_user,
-                # :current_platform_admin?,
-=======
   helper_method :logged_in?,
                 :current_user,
                 :current_dashboard_path,
->>>>>>> revert permissions
                 :all_categories,
                 :cart
 
