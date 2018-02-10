ce03e78a941077179f16aafed58b3bb0213473b0
abarnes26 <abarnes26@yahoo.com>
2018-02-10 14:51:09 -0700

    admin_approves

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,8 @@
 class Store < ApplicationRecord
   has_many :items
   has_many :user_roles
+  scope :ordered_by_id, -> {order(:id)}
+
   enum status: ['pending', 'active', 'suspended']
 
   validates_presence_of :name
