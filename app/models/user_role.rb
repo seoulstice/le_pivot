ff4f92117b46ba1796f24317a97ad87258fb0e2c
seoulstice <jungyoungjoon@gmail.com>
2018-02-10 10:59:28 -0700

    Refactor nav

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,5 +1,5 @@
 class UserRole < ApplicationRecord
   belongs_to :user, inverse_of: :user_roles
   belongs_to :store, optional: true, inverse_of: :user_roles
-  belongs_to :role, inverse_of: :user_roles
+  belongs_to :role
 end
