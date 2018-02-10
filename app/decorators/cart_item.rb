f3159a5a4ae326e0d769c5e1edf67edc86538b4b
Josi McClellan <josiahmcclellan07@gmail.com>
2018-02-10 11:15:36 -0700

    indentation

--- app/decorators/cart_item.rb
+++ app/decorators/cart_item.rb
@@ -1,10 +1,10 @@
 class CartItem < SimpleDelegator
-    attr_reader :quantity, :subtotal
+  attr_reader :quantity, :subtotal
 
-    def initialize(id, quantity)
-      item_to_wrap = Item.find(id)
-      super(item_to_wrap)
-      @quantity = quantity
-      @subtotal = quantity * price
-    end
+  def initialize(id, quantity)
+    item_to_wrap = Item.find(id)
+    super(item_to_wrap)
+    @quantity = quantity
+    @subtotal = quantity * price
   end
+end
