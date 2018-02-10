974a6fb7c92247f1064ea5f1349981f3f70603bb
seoulstice <jungyoungjoon@gmail.com>
2018-02-10 15:50:55 -0700

    Refactor .create_store method to update slug on store

--- app/models/store_creator.rb
+++ app/models/store_creator.rb
@@ -2,7 +2,8 @@ class StoreCreator
 
   def self.create_store(user, store_params)
     role = Role.find_by(name: "store_admin")
-    store = Store.create(store_params, slug: store_params[:name].parameterize)
+    store = Store.create(store_params)
+    store.update(slug: store_params[:name].parameterize)
     UserRole.create(user: user, store: store, role: role )
     store
   end
