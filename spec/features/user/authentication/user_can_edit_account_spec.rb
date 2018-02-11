180992d16a8342893340271fc4c3a0d6a90c060a
seoulstice <jungyoungjoon@gmail.com>
2018-02-11 12:16:55 -0700

    Added to user_edit_account spec

--- spec/features/user/authentication/user_can_edit_account_spec.rb
+++ spec/features/user/authentication/user_can_edit_account_spec.rb
@@ -1,7 +1,22 @@
 require 'rails_helper'
 
 describe "When a registered User" do
-  context "navigate to account edit form" do
+  context "navigates to account edit form" do
+    it "the User can edit their account without entering a password" do
+      user = create(:user, name: "Gob")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
+      visit edit_user_path
+
+      fill_in "user[name]", with: "George Oscar Bluth"
+      click_on "Submit"
+
+      expect(current_path).to eq(edit_user_path)
+      expect(page).to have_content("George Oscar Bluth")
+      expect(page).to have_content("Successfully updated your account information.")
+
+
+
+    end
   end
 end
