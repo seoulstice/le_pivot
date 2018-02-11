786d101c3573d0555ac34f62b5740782a831a883
seoulstice <jungyoungjoon@gmail.com>
2018-02-11 13:24:56 -0700

    Removed spec from other branch

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211182429) do
+ActiveRecord::Schema.define(version: 20180211191248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
