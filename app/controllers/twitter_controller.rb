9ea86d3092ed9cd0af181dffb5d0b40c4a7d0de6
Josi McClellan <josiahmcclellan07@gmail.com>
2018-02-11 11:37:01 -0700

    extract twitter account info to seperate table

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -4,20 +4,32 @@ class TwitterController < ApplicationController
   end
 
   def create
-    current_user.send_tweet(params[:text])
+    current_user.twitter_account.tweet(params[:text])
     redirect_to current_dashboard_path
   end
 
   def update
-    current_user.add_twitter_credentials(request.env['omniauth.auth'])
-    twitter_successful
+    data = extract_account_data(request.env['omniauth.auth'])
+    if build_account(data).save
+      flash_success "Successfully paired Twitter account for #{current_user.first_name} #{current_user.last_name}"
+      redirect_to current_dashboard_path
+    else
+      flash[:error] = 'Something went wrong and we were unable to conntect your account.'
+      redirect_to current_dashboard_path
+    end
   end
 
   private
 
-    def twitter_successful
-      flash[:notice] = "Successfully paired Twitter account for #{current_user.first_name} #{current_user.last_name}"
-      redirect_to current_dashboard_path
+    # I get UID for uniqueness
+    # , but why do we need their screen name?
+    def build_account(raw_data)
+      current_user.build_twitter_account(
+        uid:                  raw_data.uid
+        screen_name:          raw_data.extra.raw_info.screen_name
+        oauth_token:          raw_data.credentials.token
+        oauth_token_secret:   raw_data.credentials.secret
+      )
     end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -91,6 +91,14 @@ ActiveRecord::Schema.define(version: 20180211191248) do
     t.index ["slug"], name: "index_stores_on_slug", unique: true
   end
 
+  create_table "twitter_accounts", force: :cascade do |t|
+    t.string "uid"
+    t.string "screen_name"
+    t.string "oauth_token"
+    t.string "oauth_token_secret"
+    t.index ["uid"], name: "index_twitter_accounts_on_uid", unique: true
+  end
+
   create_table "user_roles", force: :cascade do |t|
     t.bigint "user_id"
     t.bigint "store_id"
@@ -110,11 +118,6 @@ ActiveRecord::Schema.define(version: 20180211191248) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "screen_name"
-    t.string "uid"
-    t.string "oauth_token"
-    t.string "oauth_token_secret"
-    t.string "username"
   end
 
   add_foreign_key "items", "categories"
