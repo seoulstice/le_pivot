b80774875d32bb249184b43f80786528918361ee
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-11 15:46:52 -0700

    Set up for chatrooms and modify create action in chatroom controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,10 +3,11 @@ class MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
     message.user = current_user
-    if !message.save
-      redirect_to chatrooms_path
-    else
-      message.save
+    if message.save
+      ActionCable.server.broadcast 'messages',
+        message: message.content,
+        user: message.user.username
+      head :ok
     end
   end
 
