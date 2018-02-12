388cf4175d191e600c9d30f152afb1ba2b829bf1
seoulstice <jungyoungjoon@gmail.com>
2018-02-12 12:31:53 -0700

    VCR implementation

--- app/models/order_creator.rb
+++ app/models/order_creator.rb
@@ -8,21 +8,4 @@ class OrderCreator
     order
   end
 
-  def self.create_order(user, cart)
-    user.orders.create(
-      total_price: cart.total_price,
-      order_items_attributes: cart.items.map{ |item| {
-        item_id: item.id, quantity: item.quantity
-      }}
-    )
-  end
-
-  def self.create_order(user, cart)
-    order_items = cart.order_items
-    user.orders.create(
-      order_items: order_items,
-      total_price: cart.total_price
-    )
-  end
-
 end
