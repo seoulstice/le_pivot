333e9030218961a366414bd1d6f8f362c327b92f
seoulstice <jungyoungjoon@gmail.com>
2018-02-12 12:31:35 -0700

    Added configuration for VCR

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,10 +12,18 @@ require 'support/simple_cov'
 require 'feature_helper'
 require 'santas_little_helper'
 require './db/seeder'
+require 'webmock/rspec'
+require 'vcr'
 
 ActiveRecord::Migration.maintain_test_schema!
 
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/cassettes"
+  config.hook_into :webmock
+end
+
 DatabaseCleaner.strategy = :truncation
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include SantasLittleHelper
