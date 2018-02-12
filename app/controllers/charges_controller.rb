9056b75e27ceb0089b3210702f8418b0ac2ac38b
abarnes26 <abarnes26@yahoo.com>
2018-02-12 12:34:07 -0700

    completed refactor of stripe and added css

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -13,7 +13,10 @@ class ChargesController < ApplicationController
 
     rescue Stripe::CardError => e
       flash[:error] = e.message
-      redirect_to new_charge_path
+      redirect_to thanks_path
+  end
+
+  def thanks
   end
 
   def thanks
