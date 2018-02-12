f265167c1e6260055bddf73fefead757b93e0862
seoulstice <jungyoungjoon@gmail.com>
2018-02-12 12:30:22 -0700

    Create usps_service_spec

--- spec/services/usps_service_spec.rb
+++ spec/services/usps_service_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe UspsService do
   describe "instance methods" do
+<<<<<<< HEAD
     let(:usps_service) { UspsService.new({"container_type"=>"SM FLAT RATE BOX", "zip"=>"90201"}) }
 
     it "#shipping_cost_api" do
@@ -9,6 +10,13 @@ describe UspsService do
         shipping_estimate = usps_service.get_rate_calculation_api
 
         expect(shipping_estimate).to be_a(String)
+=======
+    let(:usps_service) { UspsService.new}
+
+    it "#shipping_cost_api" do
+      VCR.user_cassette("shipping_cost_estimate") do
+        
+>>>>>>> Create usps_service_spec
       end
     end
   end
