0a09447c47668e9a593ec404ae4797dd087466bc
seoulstice <jungyoungjoon@gmail.com>
2018-02-12 19:06:11 -0700

    Refactor usps_service_spec.rb

--- spec/services/usps_service_spec.rb
+++ spec/services/usps_service_spec.rb
@@ -5,8 +5,11 @@ describe UspsService do
     let(:usps_service) { UspsService.new}
 
     it "#shipping_cost_api" do
-      VCR.user_cassette("shipping_cost_estimate") do
-        
+      VCR.use_cassette("shipping_cost_estimate") do
+        shipping_estimate = usps_service.get_rate_calculation_api
+# binding.pry
+        expect(shipping_estimate).to be_a(String)
+
       end
     end
   end
