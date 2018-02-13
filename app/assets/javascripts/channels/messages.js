fe912e1600e0c16c54c8f5d61fdfb903d35b5680
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-12 17:40:59 -0700

    Javascript for action able file

--- app/assets/javascripts/channels/messages.js
+++ app/assets/javascripts/channels/messages.js
@@ -1,9 +1,13 @@
-App.messages = App.cable.subscriptions.create("MessagesChannel", { received: function(data){
-  $("#messages").removeClass('hidden')
-  return $('#messages').append(this.renderMessage(data));
-},
+App.messages = App.cable.subscriptions.create({
+  channel: "MessagesChannel",
+  room: $('#message_chatroom_id').val()
+}, {
+  received: function(data) {
+    $("#messages").removeClass('hidden')
+    return $('#messages').append(this.renderMessage(data));
+  },
 
-renderMessage: function(data) {
-  return "<p> <b>" + data.user + ": </b>" + data.message + "</p>":
-}
+  renderMessage: function(data) {
+    return `<p><b>${data.user}:</b> ${data.message}</p>`;
+  }
 });
