7a772e49d2e6dfb2258d543855b1909074dc78d7
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-12 17:41:59 -0700

    clean up fiel and add username to user params

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
     message = Message.new(message_params)
     message.user = current_user
     if message.save
-      ActionCable.server.broadcast 'messages',
+        ActionCable.server.broadcast 'messages',
         message: message.content,
         user: message.user.username
       head :ok
