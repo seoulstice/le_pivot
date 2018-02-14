b2cba2fbb852db46ed2ea905bcfaf72eb888fc3e
seoulstice <jungyoungjoon@gmail.com>
2018-02-14 14:10:00 -0700

    Made changes in usps spec

--- spec/services/usps_service_spec.rb
+++ spec/services/usps_service_spec.rb
@@ -2,14 +2,13 @@ require 'rails_helper'
 
 describe UspsService do
   describe "instance methods" do
-    let(:usps_service) { UspsService.new}
+    let(:usps_service) { UspsService.new }
 
     it "#shipping_cost_api" do
       VCR.use_cassette("shipping_cost_estimate") do
         shipping_estimate = usps_service.get_rate_calculation_api
-# binding.pry
-        expect(shipping_estimate).to be_a(String)
 
+        expect(shipping_estimate).to be_a(String)
       end
     end
   end
