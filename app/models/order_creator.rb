10f23255ec06db393f314860cd9999a2c51d26b2
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 13:00:54 -0700

    Refactor order_creator

--- app/models/order_creator.rb
+++ app/models/order_creator.rb
@@ -1,7 +1,9 @@
 class OrderCreator
 
-  def self.create_order(user, cart)
-    order = Order.create(total_price: cart.total_price, user: user)
+
+  def self.create_order(user, cart, shipping_params)
+    total = cart.total_price + shipping_params.get_rate_calculation_api.to_f
+    order = Order.create(total_price: cart.total_price, total_price_with_shipping: total ,user: user)
     cart.items.each do |item|
       OrderItem.create(item_id: item.id, order: order, quantity: item.quantity)
     end
