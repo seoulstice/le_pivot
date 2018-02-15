8a270d168e0fe5481e98b6a6c64682354b858af2
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 16:25:48 -0700

    Refactor user_spec

--- spec/features/user/items/user_can_place_orders_spec.rb
+++ spec/features/user/items/user_can_place_orders_spec.rb
@@ -9,19 +9,6 @@ RSpec.feature "User can place an order" do
     click_on "Add to cart"
   end
 
-  it "and after placing order, sees message 'order was successfully placed" do
-    VCR.use_cassette("order_checkout") do
-      visit cart_path
-      choose("container_type_SM_FLAT_RATE_BOX", visible: false)
-      fill_in "zip", with: "90210"
-      click_on "Calculate Shipping Cost"
-      click_on "Checkout"
-
-      expect(current_path).to eq(orders_path)
-      expect(page).to have_content("Order was successfully placed")
-    end
-  end
-
   it "and estimate shipping cost" do
     VCR.use_cassette("shipping_feature") do
       visit cart_path
@@ -29,9 +16,8 @@ RSpec.feature "User can place an order" do
       choose("container_type_SM_FLAT_RATE_BOX", visible: false)
       fill_in "zip", with: "90210"
       click_on "Calculate Shipping Cost"
-      click_on "Checkout"
-      expect(current_path).to eq(orders_path)
 
+      expect(current_path).to eq(new_charge_path)
     end
   end
 end
