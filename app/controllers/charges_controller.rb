81ff6e26ceb5a3d564b71222bb2e414014dec99e
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 16:26:42 -0700

    Refactor charges#new

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,10 +1,11 @@
 class ChargesController < ApplicationController
 
   def new
+    @usps_cost = usps_cost
   end
 
   def create
-    @order = OrderCreator.create_order(current_user, cart)
+    @order = OrderCreator.create_order(current_user, cart, params[:shipping])
     if @order.save then proceed else reset end
   end
 
@@ -12,6 +13,7 @@ class ChargesController < ApplicationController
   end
 
   private
+    attr_reader :usps_cost
 
     def charge_customer
       StripeServices.new(params[:stripeEmail], params[:stripeToken]).charge
@@ -32,4 +34,11 @@ class ChargesController < ApplicationController
       redirect_to new_charge_path
     end
 
+    def shipping_params
+      params.permit(:container_type, :zip)
+    end
+
+    def usps_cost
+      @usps = (UspsService.new(shipping_params).get_rate_calculation_api.to_f) * 100
+    end
 end
