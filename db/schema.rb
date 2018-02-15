0c95eef7e39b805c05c3dc65ec678840ed700258
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-15 15:08:58 -0700

    add username migration to schema for user

--- db/schema.rb
+++ db/schema.rb
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20180214034612) do
     t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
   add_foreign_key "items", "categories"
