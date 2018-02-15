d436bfec8421b0bd83171e958bd95335e982930e
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 16:29:03 -0700

    Changed bc of migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215193638) do
+ActiveRecord::Schema.define(version: 20180215001622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -72,7 +72,6 @@ ActiveRecord::Schema.define(version: 20180215193638) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "total_price"
-    t.string "image"
     t.float "total_price_with_shipping"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
