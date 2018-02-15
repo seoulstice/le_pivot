9648cb9510bb3ccaba82a28a9aed47501f499844
Josi McClellan <josiahmcclellan07@gmail.com>
2018-02-15 16:35:18 -0700

    initial app routing restructure for role tiers

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,62 @@
 class ItemsController < ApplicationController
 
+  before_action :set_store, except: :show
+
 	def show
 		@item = ItemDecorator.new(Item.find(params[:id]))
 	end
+
+  def new
+    @item = @store.items.new
+  end
+
+  def create
+    @item = @store.items.new(item_params)
+    if try_save(@item, store_path(@store), nil)
+      flash_success "New item #{success_link(@item)} added."
+    end
+  end
+
+  def update
+    @item = @store.items.find(params[:id])
+    @item.assign_attributes(item_params)
+    try_save(@item, store_path(@store), "Item #{success_link(@item)} updated.")
+  end
+
+  def edit
+    @item = @store.items.find(params[:id])
+  end
+
+  private
+
+    def item_params
+      upload_or_default_image_param
+      params.require(:item).permit(:title, :description, :price, :image, :category_id)
+    end
+
+    def upload_or_default_image_param
+      params[:item][:image] &&= upload_image['url']
+      params[:item][:image] ||= "http://res.cloudinary.com/tyjoo27/image/upload/v1518213324/cp82cjhrlplyxbet4gko.png"
+    end
+
+    def upload_image
+      Cloudinary::Uploader.upload(params[:item][:image].tempfile.path, auth)
+    end
+
+    def auth
+      {
+        cloud_name: ENV['CLOUD_NAME'],
+        api_key:    ENV['CLOUDINARY_API_KEY'],
+        api_secret: ENV['CLOUDINARY_API_SECRET']
+      }
+    end
+
+    def success_link(item)
+      "<a href=#{(item_path(item))}>#{item.title}</a>"
+    end
+
+    def set_store
+      @store = Store.find_by_slug!(params[:store_slug])
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -39,9 +39,9 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resource :dashboard, only: :show
     resource :analytics, only: :show
-    resources :items, only: [:index, :edit, :new, :create, :update]
+    resources :orders, only: [:index, :update]
+    resources :stores, only: [:index, :update]
   end
 
   namespace :api do
@@ -55,8 +55,9 @@ Rails.application.routes.draw do
   resources :twitter_accounts, only: :create
 
   resources :stores, only: [:new, :create, :index]
-  resources :stores, only: [:update, :show], path: '/', param: :slug do
+  resources :stores, only: [:show, :edit, :update], path: '/', param: :slug do
     resources :tweets, only: [:new, :create]
+    resources :items, only: [:new, :edit, :update, :create]
   end
 
 end
