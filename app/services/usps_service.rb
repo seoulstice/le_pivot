8cbc04bd7774cba342be02a8fdaadbeb82c182b5
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 13:00:22 -0700

    Refactor random weight methods

--- app/services/usps_service.rb
+++ app/services/usps_service.rb
@@ -2,12 +2,12 @@ class UspsService
   require 'faraday_middleware'
   attr_reader :params
 
-  def initialize
+  def initialize(params)
     @params = params
   end
 
   def get_rate_calculation_api
-    get_xml("/ShippingApi.dll", usps_shipping_rate_params )
+    get_xml("/ShippingApi.dll", usps_shipping_rate_params ).body["RateV4Response"]["Package"]["Postage"]["Rate"]
   end
 
   private
@@ -23,7 +23,6 @@ class UspsService
 
     def get_xml(url, query_params = {})
       response = conn.get(url, query_params)
-      binding.pry
     end
 
     def usps_shipping_rate_params
@@ -33,10 +32,9 @@ class UspsService
     end
 
     def xml_shipping_rate_request_params
-      # binding.pry
      "<RateV4Request USERID='#{ENV["USPS_KEY"]}'>
         <Package ID='1ST'>
-          <Service>#{usps_service_types}</Service>
+          <Service>PRIORITY</Service>
           <ZipOrigination>80211</ZipOrigination>
           <ZipDestination>#{destination_zip}</ZipDestination>
           <Pounds>#{random_pounds}</Pounds>
@@ -47,26 +45,19 @@ class UspsService
       </RateV4Request>"
     end
 
-    def usps_service_types
-      ["PRIORITY", "PRIORITY MAIL EXPRESS"].sample
-      # params[service_type]
-    end
-
     def destination_zip
-      # params[zip_destination]
-      19072
+      params["zip"]
     end
 
     def random_pounds
-      (1..70).to_a.sample
+      (0..70).to_a.sample
     end
 
     def random_ounces
-      (1...16).to_a.sample
+      (0...16).to_a.sample
     end
 
     def random_container
-      ["SM FLAT RATE BOX", "MD FLAT RATE BOX",
-      "LG FLAT RATE BOX"].sample
+      params["container_type"]
     end
 end
