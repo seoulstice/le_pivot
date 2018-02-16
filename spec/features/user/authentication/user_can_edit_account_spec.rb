ee7f679f91f32aadeeb094ebc45f9a9bbb54e084
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-15 22:23:21 -0700

    modify test to fix it by fixign capybara selector

--- spec/features/user/authentication/user_can_edit_account_spec.rb
+++ spec/features/user/authentication/user_can_edit_account_spec.rb
@@ -5,15 +5,15 @@ describe "When a registered User" do
     it "the User can edit their account without entering a password" do
       user = create(:user, first_name: "Gob")
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
       visit edit_user_path
-      expect(page).to have_content("Gob")
+
+      expect(page).to have_field('user[first_name]', with: 'Gob')
 
       fill_in "user[first_name]", with: "George Oscar Bluth"
       click_on "Submit"
 
       expect(current_path).to eq(edit_user_path)
-      expect(page).to have_content("George Oscar Bluth")
+      expect(page).to have_field('user[first_name]', with: 'George Oscar Bluth')
       expect(page).to have_content("Successfully updated your account information.")
     end
   end
