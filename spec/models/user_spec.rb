fd0b0a60a7888531749dacc117fadf22403d91a2
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-15 17:04:06 -0700

    create user validations and relationsihps

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,4 +30,52 @@ describe "validations" do
     end
   end
 
+  context "valid attributes" do
+    it "is valid only with valid attributes" do
+      user = User.new(first_name: "tj", last_name: "joo", email: "a@a.com", password: '123')
+      expect(user).to be_valid
+    end
+  end
+
+  context "relationships" do
+    scenario "a user has a relatiosnhip with orders" do
+      user = build(:user)
+      expect(user).to respond_to(:orders)
+    end
+
+    scenario "a user has a relatiosnhip with user_roles" do
+      user = build(:user)
+      expect(user).to respond_to(:user_roles)
+    end
+
+    scenario "a user has a relatiosnhip with roles" do
+      user = build(:user)
+      expect(user).to respond_to(:roles)
+    end
+
+    scenario "a user has a relatiosnhip with stores" do
+      user = build(:user)
+      expect(user).to respond_to(:stores)
+    end
+
+    scenario "a user has a relatiosnhip with api_key" do
+      user = build(:user)
+      expect(user).to respond_to(:api_key)
+    end
+
+    scenario "a user has a relatiosnhip with twitter_account" do
+      user = build(:user)
+      expect(user).to respond_to(:twitter_account)
+    end
+
+    scenario "a user has a relatiosnhip with messages" do
+      user = build(:user)
+      expect(user).to respond_to(:messages)
+    end
+
+    scenario "a user has a relatiosnhip with chatrooms" do
+      user = build(:user)
+      expect(user).to respond_to(:chatrooms)
+    end
+  end
 end
