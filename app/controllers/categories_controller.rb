0e629dc8cad05195fd192d56171883d3b0b8d6b5
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-15 20:28:51 -0700

    modify categoires controller to add wil paginate method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
 	def show
 		@category = Category.friendly.find(params[:category_slug])
+		@items = @category.items.paginate(page: params[:page], :per_page => 20)
 	end
 end
