2ef3cd4c235835e1fa3db1550b5190a48e51bed4
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 18:19:24 -0700

    Fix merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215001622) do
+ActiveRecord::Schema.define(version: 20180215193638) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,7 +128,6 @@ ActiveRecord::Schema.define(version: 20180215001622) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "phone"
-    t.string "username"
   end
 
   add_foreign_key "items", "categories"
