d0e0216504417c6c025a4ec47dd2e663ff97b162
Tyjoo26 <Tyjoo21@gmail.com>
2018-02-15 20:59:30 -0700

    Modify add to cart link to add item

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,7 +1,7 @@
 module ItemsHelper
   def add_to_cart_button(item)
     if item.active?
-      button_to "Add to cart", cart_path(item_id: item.id), class: badge(:primary)
+      link_to "Add to cart", cart_path(item_id: item.id), method: "post", class: badge(:primary)
     else
       content_tag :div, 'Retired', class: badge(:secondary)
     end
