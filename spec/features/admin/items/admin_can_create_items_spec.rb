e6741976731c80eb0a3b9fe784d15eb3f1d433e9
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 19:27:54 -0700

    refactor test to pass using VCR

--- spec/features/admin/items/admin_can_create_items_spec.rb
+++ spec/features/admin/items/admin_can_create_items_spec.rb
@@ -10,16 +10,18 @@ feature 'Admin Item Management' do
     end
 
     it 'Admin can create an item with an image' do
-      click_on 'Create New Item'
-      fill_in 'item[title]', with: 'Onesie'
-      fill_in 'item[description]', with: 'This Onesie is awesome!'
-      fill_in 'item[price]', with: '59.99'
-      page.attach_file('item[image]', testing_image)
-      click_on 'Create Item'
+      VCR.use_cassette("admin_create_item_cassette") do
+        click_on 'Create New Item'
+        fill_in 'item[title]', with: 'Onesie'
+        fill_in 'item[description]', with: 'This Onesie is awesome!'
+        fill_in 'item[price]', with: '59.99'
+        page.attach_file('item[image]', testing_image)
+        click_on 'Create Item'
 
-      expect(current_path).to eq(admin_items_path)
-      expect(page).to have_content('Onesie')
-      expect(page).to have_content('59.99')
+        expect(current_path).to eq(admin_items_path)
+        expect(page).to have_content('Onesie')
+        expect(page).to have_content('59.99')
+      end
     end
 
     it 'Admin can create an item with no image' do
@@ -37,17 +39,19 @@ feature 'Admin Item Management' do
 
   describe 'updating' do
     it 'Admin can edit an item' do
-      create(:item)
-      visit admin_items_path
-      click_on 'Edit'
-      fill_in 'item[title]', with: 'White Cat Twosie'
-      fill_in 'item[description]', with: 'two is better'
-      fill_in 'item[price]', with: '39.99'
-      page.attach_file('item[image]', testing_image)
-      click_on 'Update'
+      VCR.use_cassette("admin_updating_item_cassette") do
+        create(:item)
+        visit admin_items_path
+        click_on 'Edit'
+        fill_in 'item[title]', with: 'White Cat Twosie'
+        fill_in 'item[description]', with: 'two is better'
+        fill_in 'item[price]', with: '39.99'
+        page.attach_file('item[image]', testing_image)
+        click_on 'Update'
 
-      expect(page).to have_content('White Cat Twosie')
-      expect(page).to have_content('39.99')
+        expect(page).to have_content('White Cat Twosie')
+        expect(page).to have_content('39.99')
+      end
     end
   end
 
