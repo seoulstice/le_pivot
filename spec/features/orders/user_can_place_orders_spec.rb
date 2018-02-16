1ae276397f15b638db4cb496e38ac3f3e5a18b10
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 20:52:17 -0700

    Refactor user_can_place_orders_spec

--- spec/features/orders/user_can_place_orders_spec.rb
+++ spec/features/orders/user_can_place_orders_spec.rb
@@ -1,16 +1,12 @@
 require 'rails_helper'
 
 RSpec.feature "User can place an order" do
-  it "and see the message 'order was successfully placed'" do
-
-    item = create(:item)
-    stub_logged_in_user
-    visit item_path(item)
-    click_on "Add to cart"
-  end
 
   it "and estimate shipping cost" do
     VCR.use_cassette("shipping_feature") do
+      user = create(:user, first_name: "Gob")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
       visit cart_path
 
       choose("container_type_SM_FLAT_RATE_BOX", visible: false)
