505bf80dbae85bdc4394ed6ecfd623d0d205dc14
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 19:19:46 -0700

    no changes

--- spec/models/order_creator_spec.rb
+++ spec/models/order_creator_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 RSpec.describe OrderCreator do
   context "when creating an order" do
-    it "saves cart#total price to the total price attribute on a Order object" do
-      params = {"container_type"=>"SM FLAT RATE BOX", "zip"=>"90201"}
-      item_1 = create(:item, price: 1.00)
-      item_2 = create(:item, price: 4.95)
-      cart = Cart.new( item_1.id => 2, item_2.id => 1 )
-      oc = OrderCreator.create_order(create(:user), cart , params )
+    VCR.use_cassette("order_creator_spec") do
+      it "saves cart#total price to the total price attribute on a Order object" do
+        store = create(:store)
+        item_1 = create(:item, price: 1.00, store: store)
+        item_2 = create(:item, price: 4.95, store: store)
+        cart = Cart.new( item_1.id => 2, item_2.id => 1 )
+        oc = OrderCreator.create_order(create(:user), cart, 0.0)
 
-      expect(oc.total_price).to eq(6.95)
+        expect(cart.total_price).to eq(6.95)
+      end
     end
   end
 end
