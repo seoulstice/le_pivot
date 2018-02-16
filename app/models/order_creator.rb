f4f1b8b37e51aea875ecd1e61f9655fcf98ebcfb
seoulstice <jungyoungjoon@gmail.com>
2018-02-15 19:19:46 -0700

    no changes

--- app/models/order_creator.rb
+++ app/models/order_creator.rb
@@ -4,10 +4,10 @@ class OrderCreator
   def self.create_order(user, cart, shipping_params)
     total = cart.total_price + shipping_params
     order = Order.create(total_price: cart.total_price, total_price_with_shipping: total, user: user)
-    cart.items.each do |item|
+    cart.order_items.each do |item|
       OrderItem.create(item_id: item.id, order: order, quantity: item.quantity)
     end
-    order
+    # order
   end
 
 end
